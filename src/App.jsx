ffee1d9c49e6f76171a8a4f2334bca0565fe88e8
Sergio Ruiz <sergioruizrock@gmail.com>
2024-02-19 21:58:50 -0300

    proyecto listo para desarrollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <>
+    <header className='bg-dark text-center text-light py-3'>
+      <h1>titulo</h1>
+    </header>
+    <Container className='mainContainer'></Container>
+    <footer className='bg-dark text-center text-light py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
